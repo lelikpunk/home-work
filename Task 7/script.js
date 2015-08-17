ffc1e709a3cca897201c525d754b85dbc70398af
lelikpunk <lelikpunk@gmail.com>
2015-08-17 16:13:02 +0300

    add task 7

---
+++ Task 7/script.js
@@ -0,0 +1,47 @@
+var myPerson = {
+	name: 'lelik',
+	age: 26,
+	gender: 'male',
+	favouriteGame: 'Dota 2',
+	profession: 'QA',
+	language: 'JavaScript',
+	foodName: 'burger',
+	sayHi: function() {
+		return 'Привет, меня зовут ' + this.name;
+	},
+	sayAge: function() {
+		return 'Мне ' + this.age + ' лет';
+	},
+	sayGender: function() {
+		return 'Мой пол ' + this.gender;
+	},
+	playGame: function() {
+		return 'Я играю в ' + this.favouriteGame;
+	},
+	myProf: function() {
+		return 'Моя из профессий ' + this.profession;
+	},
+	study: function() {
+		return 'Я учу язык программирования ' + this.language;
+	},
+	eat: function() {
+		return 'Я люблю есть ' + this.foodName;
+	}
+}
+
+var food = {
+	foodName: 'cheeseburger',
+	cook: function() {
+		return this.name + 'prepares!';
+	}
+}
+
+
+
+
+
+
+console.log(myPerson.name);
+console.log(myPerson.age);
+console.log(myPerson.sayHi());
+console.log(myPerson.eat.call(food));
