9d4d3b1c1bd2311540a377a417f8221077f5de81
lelikpunk <lelikpunk@gmail.com>
2015-08-24 03:27:17 +0300

    fix test 6

--- Test code Home/script.js
+++ Test code Home/script.js
@@ -1,12 +1,45 @@
-var person = {
-	name: 'lelik',
-	age: 26,
-	gender: 'male',
-	sayHi: function() {
-		console.log('Hi, my name is ' + this.name);
+var arr1 = ['А', 'Р', 'Б', 'У', 'З'];
+var	arr2 = ['_', '_', '_', '_', '_'];
+
+var game = {
+	letter: function() {
+		
+		var let = prompt('Введите букву');
+		game.chekLet(arr1, let);
+	},
+
+	chekLet: function(arr, let) {
+		var chekOneLet = false;
+		var chekArr = false;
+		arr.forEach(function(elem, i, arr) {
+			if (elem == let) {
+			arr2[i] = let;
+			chekOneLet = true;
+			}
+			if (arr2[i] == '_') {
+				chekArr = true;
+			}
+		});
+		if (chekOneLet) {
+		console.log('Вы угадали букву');
+		console.log(arr2.join(' '));
+		if (!chekArr) {
+			console.log('Вы выиграли');
+		} else {
+			game.letter();
+		}
+	} else {
+		console.log('Нет, не верно');
+		game.letter();
 	}
-};
+	}
+	
+
+}
 
 
-var personData = JSON.stringify(person);
-console.log(personData);
+
+
+
+game.letter();
+
