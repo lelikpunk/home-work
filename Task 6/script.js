ecf29bfcda687796e8ca9792e25e49b965ee3431
lelikpunk <lelikpunk@gmail.com>
2015-08-26 20:43:25 +0300

    fix 6 task

--- Task 6/script.js
+++ Task 6/script.js
@@ -1,27 +1,44 @@
 var arr1 = ['А', 'Р', 'Б', 'У', 'З'];
-var arr2 = ['_', '_', '_', '_', '_'];
+var	arr2 = ['_', '_', '_', '_', '_'];
 
-function chekLet(letter) {
-	var chekOneLet = false;
-	var chekArr = false;
-	var len = arr1.length;
-	for (var i = 0; i < len; i++) {
-		if (arr1[i] == letter) {
-			arr2[i] = letter;
+var game = {
+	letter: function() {
+		
+		var let = prompt('Введите букву');
+		game.chekLet(arr1, let);
+	},
+
+	chekLet: function(arr, let) {
+		var chekOneLet = false;
+		var chekArr = false;
+		arr.forEach(function(elem, i, arr) {
+			if (elem == let) {
+			arr2[i] = let;
 			chekOneLet = true;
-		}
-		if (arr2[i] == '_') {
-			chekArr = true;
-		}
-	}
-	if (chekOneLet) {
+			}
+			if (arr2[i] == '_') {
+				chekArr = true;
+			}
+		});
+		if (chekOneLet) {
 		console.log('Вы угадали букву');
 		console.log(arr2.join(' '));
 		if (!chekArr) {
 			console.log('Вы выиграли');
+		} else {
+			game.letter();
 		}
 	} else {
 		console.log('Нет, не верно');
+		game.letter();
 	}
+	}
+	
+
 }
 
+
+
+
+
+game.letter();
