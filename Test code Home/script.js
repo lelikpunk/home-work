49b2a4802f4c3b267a7fd4f6a1b05c0d5fff48bf
lelikpunk <lelikpunk@gmail.com>
2015-08-29 20:22:31 +0300

    add task 8

--- Test code Home/script.js
+++ Test code Home/script.js
@@ -1,47 +1,34 @@
-var circle = {
-  diameter : '100px',
-  color : 'red',
-  element: document.createElement('div'),
-  render: function render () {
-    var el = this.element;
-    el.style.height = this.diameter;
-    el.style.width = this.diameter;
-    el.style.background = 'red';
-    el.style.borderRadius = '50%';
-    el.style.position = 'absolute';
-    el.style.top = '0';
-    el.style.left = '0';
-    console.log('Выводим круг на страницу');
-    document.body.appendChild(el);
-  },
-  move: function move (where) {
-    console.log('Выводим круг на страницу');
-    var tempSize = 0;
-    switch(where) {
-      case 'down':
-        tempSize =  parseInt(this.element.style.top, 10);
-        tempSize = tempSize + 100;
-        this.element.style.top = tempSize + 'px';
-        break;
-      case 'up':
-        tempSize =  parseInt(this.element.style.top, 10);
-        tempSize = tempSize - 100;
-        this.element.style.top = tempSize + 'px';
-        break;
-      case 'left':
-        tempSize =  parseInt(this.element.style.left, 10);
-        tempSize = tempSize - 100;
-        this.element.style.left = tempSize + 'px';
-        break;
-      case 'rigth':
-      default:
-        tempSize =  parseInt(this.element.style.left, 10);
-        tempSize = tempSize + 100;
-        this.element.style.left = tempSize + 'px';
-    }
-  }
-}
 
-circle.render();
-circle.move('right');
+
+
+
+var arr1 = [];
+
+function number() {
+	var nam = +prompt('Введите число');
+	if (nam) {
+		arr1.push(nam);
+		number();
+	} else {
+		sumArr(arr1);
+	}
+};
+
+
+function sumArr(arr) {
+	var sum = 0;
+	var len = arr.length;
+	for (var i = 0; i < len; i++) {
+		sum = sum + arr[i];
+	}
+	console.log(sum); 
+};
+
+number();
+
+
+
+
+
+
 
